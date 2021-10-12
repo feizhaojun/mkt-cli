126d97ce6f8fdf786e286a546baa3fdcebf4f682
Mukti <m@feizhaojun.com>
2021-10-12 11:38:31 +0800

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Start Mukti Project!");
+
+const program = require('commander');
+
+program.version('1.0.0', '-v, --version')
+  .command('init <name>')
+  .action(name => {
+    console.log(name);
+  });
+
+program.parse(process.argv);
